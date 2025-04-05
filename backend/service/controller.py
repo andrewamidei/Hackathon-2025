5c933e38837009ce43c76f5c7be60749f6fe0c1d
James Shufelt <jshufelt@uark.edu>
2025-04-04 21:48:58 -0500

    wokring changes for controller llm manager class

--- backend/service/controller.py
+++ backend/service/controller.py
@@ -4,65 +4,56 @@ import json
 
 app = Flask(__name__)
 
-
-def llmQuery(model, prompt, url='http://localhost:11434/api/generate'):
-    # Use the generate function for a one-off prompt
-
-    # stream is used to define wether items should be streamd one at at time (True) or all in one message (False)
-    data = {'model': model, 'prompt': prompt, 'stream': False}
-
-    with requests.post(url, json=data, stream=True) as response:
-        for line in response.iter_lines():
-            if line:
-                json_line = line.decode('utf-8')
-                response_data = json.loads(json_line)
-                if response_data['response']:
-                    # print(response_data['response'], end='', flush=True)
-                    return response_data['response']
+serverip: str = "192.168.8.137"
 
 
-@app.route('/api/data', methods=['GET'])
-def GetDefaultResponse():
-    # Generate the response and send it to the UI
-    model = 'smollm2:135m'  # local model
-    prompt = 'Why is the sky blue? keep your response under one paragraph'
-    url = 'http://localhost:11434/api/generate'
+class LLmanager:
+    def __init__(self, model: str, url: str):
+        self.model = model
+        self.url = url
 
-    response = llmQuery(model, prompt, url)
+    def llmQuery(self, prompt: str,) -> any:
+        # Use the generate function for a one-off prompt
 
-    # data = {'message': response}
-    # return jsonify(data)
+        # stream is used to define wether items should be streamd one at at time (True) or all in one message (False)
+        data = {'model': self.model, 'prompt': prompt, 'stream': False}
 
-    return response
+        with requests.post(self.url, json=data, stream=True) as response:
+            for line in response.iter_lines():
+                if line:
+                    json_line = line.decode('utf-8')
+                    response_data = json.loads(json_line)
+                    if response_data['response']:
+                        # print(response_data['response'], end='', flush=True)
+                        return response_data['response']
 
+    def GetDefaultResponse(self) -> any:
+        # Generate the response and send it to the UI
+        model = 'smollm2:135m'  # local model
+        prompt = 'Why is the sky blue? keep your response under one paragraph'
+        url = 'http://localhost:11434/api/generate'
 
-@app.route('/api/data', methods=['POST'])
-def PostNewQuery():
+        response = self.llmQuery(model, prompt, url)
 
-    # Parse the incoming JSON data
-    request_data = request.get_json()
-    if not request_data or 'prompt' not in request_data:
-        return jsonify({'error': 'Invalid request, "prompt" is required'}), 400
+        # data = {'message': response}
+        # return jsonify(data)
 
-    # Extract the prompt from the request
-    prompt = request_data['prompt']
+        return response
 
-    model = 'smollm2:135m'  # local model
-    url = 'http://localhost:11434/api/generate'
+    def PostNewQuery(self) -> json:
 
-    response = llmQuery(model, prompt, url)
+        # Parse the incoming JSON data
+        request_data = request.get_json()
+        if not request_data or 'prompt' not in request_data:
+            return jsonify({'error': 'Invalid request, "prompt" is required'}), 400
 
-    # Return the response as JSON
-    return jsonify({'response': response})
+        # Extract the prompt from the request
+        prompt = request_data['prompt']
 
-    # return response
+        model = 'deepseek-r1:8b'  # local model
+        url = 'http://localhost:11434/api/generate'
 
+        response = self.llmQuery(model, prompt, url)
 
-def main():
-    print("Starting the Flask app...")
-    app.run(debug=True)
-
-
-if __name__ == "__main__":
-    main()
-
+        # Return the response as JSON
+        return jsonify({'response': response})
