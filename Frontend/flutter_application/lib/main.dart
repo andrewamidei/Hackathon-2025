6617adeef8cd4fa5e2453100ff8d2d9d6db49bc0
Errorcode101 <errcode101@gmail.com>
2025-04-04 22:11:26 -0500

    Frontend Changes

--- Frontend/flutter_application/lib/main.dart
+++ Frontend/flutter_application/lib/main.dart
@@ -27,62 +27,60 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final backendBloc = BlocProvider.of<BackendBloc>(context);
 
-    const Key centerKey = ValueKey<String>('bottom-sliver-list');
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App with Python Backend'),
       ),
-      body: CustomScrollView(
-        center: centerKey,
-        slivers: <Widget>[
-          SliverList(
-            key: centerKey,
-            delegate: SliverChildBuilderDelegate(
-            childCount: 1, // do not make any duplicates
-              (BuildContext context, int index) {
-                return SafeArea(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Center(
-                          child: BlocBuilder<BackendBloc, String>(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: CustomScrollView(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  slivers: <Widget>[
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                      childCount: 1, // make 1 copy of everything
+                        (BuildContext context, int index) {
+                          return BlocBuilder<BackendBloc, String>(
                             builder: (context, state) {
                               return Text(state);
                             },
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        SizedBox(
-                          // width: 370,
-                          child: TextField(
-                            // controller: nameController,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: '',
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Center(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              // backendBloc.fetchData();
-                              backendBloc.sendData("Tell me a story");
-                            },
-                            child: Text('Fetch Data from Python Backend'),
-                          ),
-                        ),
-                      ],
+                          );
+                        }
+                      ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                // width: 370,
+                child: TextField(
+                  // controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '',
                   ),
-                );
-              }
-            ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // backendBloc.fetchData();
+                    backendBloc.sendData("Tell me a story");
+                  },
+                  child: Text('Send Message'),
+                ),
+              ),
+            ],
           ),
-        ]
-      )
+        ),
+      ),
     );
   }
 }
