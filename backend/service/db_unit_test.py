9c2f335a2a6f3fb0790eeb868656118d37df42f1
codingdodia <dhairyadodia2024@gmail.com>
2025-04-04 22:33:25 -0500

    unit test added

---
+++ backend/service/db_unit_test.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+class DB_unit_test:
+
+    def __init__(self, db):
+        self.db = db
+        self.db.connect_to_db()
+    
+
+    def add_user_test(self, username, password):
+        print("Testing for adding users with same usernames")
+        for i in range(5):
+            try:
+                self.db.add_user(username, password)
+                print("User added successfully")
+            except mysql.connector.Error as err:
+                print(f"Error: {err}")
+                print("User already exists")
+        print("Testing for adding users with same usernames")
+
+        print("Testing for adding users with different usernames")
+        for i in range(5):
+            try:
+                self.db.add_user(username + str(i), password + str(i))
+                print("User added successfully")
+            except mysql.connector.Error as err:
+                print(f"Error: {err}")
+                print("User already exists")
